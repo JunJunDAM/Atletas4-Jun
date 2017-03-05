c2579ea03dbf352ad196a7e8ec251cbc70bc6c4b
JunJunDAM <juninfo1997@gmail.com>
2017-03-05 16:56:35 +0100

    first commit

---
+++ app/src/main/java/com/taniafontcuberta/basketball/util/CustomProperties.java
@@ -0,0 +1,9 @@
+package com.taniafontcuberta.basketball.util;
+
+public class CustomProperties {
+    public static String clientId = "atletasapp";
+    public static String clientSecret = "my-secret-token-to-change-in-production";
+    public static String grantType = "password";
+    public static String scope = "read write";
+    public static String baseUrl = "http://172.16.157.33:8080";
+}
